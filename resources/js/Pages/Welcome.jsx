cdec32996759d91c2d53bcad56265981d6f5c171
Jhaseft <jhasesaat@gmail.com>
2025-12-12 07:49:45 -0400

    update: inicio with videos promo

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,9 +1,10 @@
 import Products from '@/Components/welcome/Products';
 import Layout from '@/Layouts/MainLayout';
-
+import Videos from '@/Components/welcome/videos';
 export default function Welcome({ auth }) {
     return (
         <Layout title="Exclusive|Inicio" auth={auth}>
+            <Videos/>
             <Products />
         </Layout>
     );
